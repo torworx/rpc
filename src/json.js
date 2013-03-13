a8aff54812bac58f3e5aeef74d428624c87fefdb
taoyuan <taoyuan@evymind.com>
2013-03-13 19:55:41 +0800

    Clean some comment.

--- src/json.js
+++ src/json.js
@@ -2,54 +2,8 @@
     "use strict";
     var _JSON,
         hasJSON = typeof JSON != "undefined",
-    // Firefox 3.5/Gecko 1.9 fails to use replacer in stringify properly https://bugzilla.mozilla.org/show_bug.cgi?id=509184
+        // Firefox 3.5/Gecko 1.9 fails to use replacer in stringify properly https://bugzilla.mozilla.org/show_bug.cgi?id=509184
         hasJSONStringify = hasJSON && JSON.stringify({a:0}, function(k,v){return v||1;}) == '{"a":1}';
-
-    /*=====
-     return {
-     // summary:
-     //		Functions to parse and serialize JSON
-
-     parse: function(str, strict){
-     // summary:
-     //		Parses a [JSON](http://json.org) string to return a JavaScript object.
-     // description:
-     //		This function follows [native JSON API](https://developer.mozilla.org/en/JSON)
-     //		Throws for invalid JSON strings. This delegates to eval() if native JSON
-     //		support is not available. By default this will evaluate any valid JS expression.
-     //		With the strict parameter set to true, the parser will ensure that only
-     //		valid JSON strings are parsed (otherwise throwing an error). Without the strict
-     //		parameter, the content passed to this method must come
-     //		from a trusted source.
-     // str:
-     //		a string literal of a JSON item, for instance:
-     //		`'{ "foo": [ "bar", 1, { "baz": "thud" } ] }'`
-     // strict:
-     //		When set to true, this will ensure that only valid, secure JSON is ever parsed.
-     //		Make sure this is set to true for untrusted content. Note that on browsers/engines
-     //		without native JSON support, setting this to true will run slower.
-     },
-     stringify: function(value, replacer, spacer){
-     // summary:
-     //		Returns a [JSON](http://json.org) serialization of an object.
-     // description:
-     //		Returns a [JSON](http://json.org) serialization of an object.
-     //		This function follows [native JSON API](https://developer.mozilla.org/en/JSON)
-     //		Note that this doesn't check for infinite recursion, so don't do that!
-     // value:
-     //		A value to be serialized.
-     // replacer:
-     //		A replacer function that is called for each value and can return a replacement
-     // spacer:
-     //		A spacer string to be used for pretty printing of JSON
-     // example:
-     //		simple serialization of a trivial object
-     //	|	define(["dojo/json"], function(JSON){
-     // |       var jsonStr = JSON.stringify({ howdy: "stranger!", isStrange: true });
-     //	|		doh.is('{"howdy":"stranger!","isStrange":true}', jsonStr);
-     }
-     };
-     =====*/
 
     if(hasJSONStringify){
         _JSON = {
@@ -164,71 +118,13 @@
     }
 
     _JSON.fromJson = function(/*String*/ js){
-        // summary:
-        //		Parses a JavaScript expression and returns a JavaScript value.
-        // description:
-        //		Throws for invalid JavaScript expressions. It does not use a strict JSON parser. It
-        //		always delegates to eval(). The content passed to this method must therefore come
-        //		from a trusted source.
-        //		It is recommend that you use dojo/json's parse function for an
-        //		implementation uses the (faster) native JSON parse when available.
-        // js:
-        //		a string literal of a JavaScript expression, for instance:
-        //		`'{ "foo": [ "bar", 1, { "baz": "thud" } ] }'`
-
         return eval("(" + js + ")"); // Object
     };
 
-
-    /*=====
-     dojo._escapeString = function(){
-     // summary:
-     //		Adds escape sequences for non-visual characters, double quote and
-     //		backslash and surrounds with double quotes to form a valid string
-     //		literal.
-     };
-     =====*/
     var toJsonIndentStr = "\t";
 
     _JSON.toJson = function(/*Object*/ it, /*Boolean?*/ prettyPrint){
-        // summary:
-        //		Returns a [JSON](http://json.org) serialization of an object.
-        // description:
-        //		Returns a [JSON](http://json.org) serialization of an object.
-        //		Note that this doesn't check for infinite recursion, so don't do that!
-        //		It is recommend that you use dojo/json's stringify function for an lighter
-        //		and faster implementation that matches the native JSON API and uses the
-        //		native JSON serializer when available.
-        // it:
-        //		an object to be serialized. Objects may define their own
-        //		serialization via a special "__json__" or "json" function
-        //		property. If a specialized serializer has been defined, it will
-        //		be used as a fallback.
-        //		Note that in 1.6, toJson would serialize undefined, but this no longer supported
-        //		since it is not supported by native JSON serializer.
-        // prettyPrint:
-        //		if true, we indent objects and arrays to make the output prettier.
-        //		The variable `dojo.toJsonIndentStr` is used as the indent string --
-        //		to use something other than the default (tab), change that variable
-        //		before calling dojo.toJson().
-        //		Note that if native JSON support is available, it will be used for serialization,
-        //		and native implementations vary on the exact spacing used in pretty printing.
-        // returns:
-        //		A JSON string serialization of the passed-in object.
-        // example:
-        //		simple serialization of a trivial object
-        //		|	var jsonStr = dojo.toJson({ howdy: "stranger!", isStrange: true });
-        //		|	doh.is('{"howdy":"stranger!","isStrange":true}', jsonStr);
-        // example:
-        //		a custom serializer for an objects of a particular class:
-        //		|	dojo.declare("Furby", null, {
-        //		|		furbies: "are strange",
-        //		|		furbyCount: 10,
-        //		|		__json__: function(){
-        //		|		},
-        //		|	});
 
-        // use dojo/json
         return _JSON.stringify(it, function(key, value){
             if(value){
                 var tf = value.__json__||value.json;
