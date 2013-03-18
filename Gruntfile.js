d6db0b871edea1fd58bc5a6eb35adc334147e90c
taoyuan <taoyuan@evymind.com>
2013-03-18 17:17:54 +0800

    Some code clean.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,9 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        clean: ['dist'],
+        clean: {
+            dist: ["dist"]
+        },
         concat: {
             options: {
                 separator: '\n'
@@ -62,7 +64,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-//    grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
